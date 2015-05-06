ecfcd14e7f3f9b5a70c92616d20b935f53dd498d
Denislav Temeliev <dtemeliev@yahoo.com>
2015-05-06 18:10:08 +0300

    added first task

---
+++ Advanced C#/Arrays-Lists-Stacks-Queues/SortArrayOfNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Write a program to sort an array of numbers and then print them back on the console. 
+//The numbers should be entered from the console on a single line, separated by a space.
+class SortArrayOfNumbers
+{
+    static void Main()
+    {
+        var stringArray = Console.ReadLine().Split();
+        int[] numbers = Array.ConvertAll(stringArray, int.Parse);
+        Array.Sort(numbers);
+        foreach (var num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
