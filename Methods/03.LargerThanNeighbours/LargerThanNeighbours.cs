33d040a78df6923acf94c8fd4ece244d596bab29
Denislav Temeliev <dtemeliev@yahoo.com>
2015-05-07 20:00:28 +0300

    All tasks

---
+++ Methods/03.LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,51 @@
+﻿using System;
+//Write a method that checks if the element at given position in a given array of integers is larger than its two neighbours (when such exist).
+class LargerThanNeighbours
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 4, 5, 1, 0, 5 };
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(IsLargerThanNeighbours(numbers, i));
+        }
+    }
+
+    private static bool IsLargerThanNeighbours(int[] numbers, int i)
+    {
+        if (i == 0)
+        {
+            if (numbers[i] > numbers[i + 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (i == numbers.Length - 1)
+        {
+            if (numbers[i] > numbers[i - 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (numbers[i] > numbers[i + 1] && numbers[i] > numbers[i - 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ Methods/05.ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Write a method that reverses the digits of a given floating-point number.
+class ReverseNumber
+{
+    static void Main()
+    {
+        double reversed = GetReversedNumber(double.Parse(Console.ReadLine()));
+        Console.WriteLine(reversed);
+    }
+
+    private static double GetReversedNumber(double num)
+    {
+        string str = num.ToString();
+        string result = "";
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            result += str[i];
+        }
+        return double.Parse(result);
+    }
+}
+
